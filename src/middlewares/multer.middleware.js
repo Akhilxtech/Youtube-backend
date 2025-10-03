af7c95a6e9a74fa5c55ac61a7e1374273837783b
Akhil Tiwari <akhiltiwari.dev@gmail.com>
2025-10-03 16:45:21 +0530

    add multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer"
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ 
+    storage // in es 6 if the key and vaue name are same then we right like that
+})
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary"  
+import { response } from "express";
+import fs from "fs" 
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+});
+
+const uploadOnCloudinary=async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const reponse =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file has been uploaded successfully
+        console.log("file is uploded on cloudinary",reponse.url);
+        return response;
+        
+    } catch (error) {
+        // remove locally saved temp file from local server
+
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
